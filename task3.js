54862c527109c4afbcce3c955d47a0d6b7194673
Martín <magadiflo@gmail.com>
2023-01-11 16:34:38 -0500

    Tarea 3 - petición a API

--- task3.js
+++ task3.js
@@ -1,2 +1,11 @@
-//* Task 3: using https://restcountries.eu/ API,
-//* get country where alpha3Code = col
+//* Task 3: using https://restcountries.com/v2/alpha/pe API,
+//* Obtener detalles del país con código: pe
+
+const fetchData = async () => {
+    const res = await fetch('https://restcountries.com/v2/alpha/pe');
+    const country = await res.json(); //* También es una promesa por lo que también debemos esperar (await)
+
+    console.log(country);
+}
+
+fetchData();
