5f08959cd601bc6da7f562600ac92835e2252e2d
Martín <magadiflo@gmail.com>
2023-01-11 16:19:10 -0500

    Tarea 2 - Promise

--- task2.js
+++ task2.js
@@ -1,4 +1,4 @@
-// *Task 2: Create a guessing game.
+// *Task 2: Create a guessing game. (Juego de Adivinanzas)
 
 //* User story: User can enter a number
 //* User story: The system pick a random number from 1 to 6
@@ -7,6 +7,58 @@
 //* give the user 1 point, otherwise, give the user 0 points.
 //* User story: User can decide to play the game as long as they want to
 
-const start = () => {};
+const enterNumber = () => {
+    return new Promise((resolve, reject) => {
+        const userNumber = Number(window.prompt('¿Ingrese un número entre (1-6) e inclusive?'));
+        const randomNumber = Math.floor(Math.random() * 6 + 1);
+
+        if (isNaN(userNumber)) {
+            reject(new Error('Tipo de entrada incorrecto'))
+        }
+
+        if (userNumber === randomNumber) {
+            resolve({ points: 2, userNumber, randomNumber });
+
+        } else if (userNumber + 1 === randomNumber || userNumber - 1 === randomNumber) {
+            resolve({ points: 1, userNumber, randomNumber });
+
+        } else {
+            resolve({ points: 0, userNumber, randomNumber });
+        }
+    });
+};
+
+const continueGame = () => {
+    return new Promise((resolve) => { //* Como no usamos el reject, podemos no escribir el parámetro reject
+        if (window.confirm("¿Quieres continuar?")) {
+            resolve(true);
+        } else {
+            resolve(false);
+        }
+    });
+}
+
+const handleGuess = () => {
+    enterNumber()
+        .then(result => {
+            alert(`Dice: ${result.randomNumber}, Tú dijiste: ${result.userNumber}, entonces tienes ${result.points} puntos`);
+
+            continueGame().then(result => {
+                if (result) {
+                    handleGuess();
+                } else {
+                    alert('Juego finalizado!');
+                }
+            });
+        })
+        .catch(error => alert(error));
+
+
+
+}
+
+const start = () => {
+    handleGuess();
+};
 
 start();
